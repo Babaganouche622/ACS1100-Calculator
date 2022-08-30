6377dbc2d50e30b9f2f55b0d9f2584654b9b9fee
Brian <112209983+Babaganouche622@users.noreply.github.com>
2022-08-29 19:06:27 -0700

    Add files via upload

--- calculator-assignment.py
+++ calculator-assignment.py
@@ -2,15 +2,20 @@ print("Calculating total damage output considering new build variables.")
 print("You will enter: Raw damage value of weapon, percent increase.")
 raw_damage_input = float(input("Enter raw damage value of weapon: "))
 
-
+# I made this new class to keep all my math at the top of the code.
+# I figured this would be a good thing if my code were to run long so I could find it easily.
 def new_change_number(raw_damage_input, item_damage_change):
     result = raw_damage_input * (1 + item_damage_change)
     return result
 
+# This is to start my loop.
 print("Would you like to enter a new item damage increase percent value?")
 new_item_input = input("Enter Yes/No: ")
+# I had to set the variable to an int value so I could then reasign it's value later on in the loop.
 item_damage_change = 0
 
+# I can't believe I got this loop to work! Josh and I jumped out of our seats when it printed the math. 
+# Arguably outside of the scope of the project, but I wanted a way for the number of variable entries to not matter.
 while new_item_input.upper() == "YES":
     if new_item_input.upper() == "YES":
         print("New Item Damage Modifier:")
@@ -22,9 +27,3 @@ while new_item_input.upper() == "YES":
 print("Here is your total new damage output given gear:")
 print(new_change_number(raw_damage_input, item_damage_change))
         
-# print("New Item Damage Modifier")
-# item_damage_1_input = input("Enter % value of damage increase")
-# # This line conversts the STR into a FLOAT so we can apply the math.
-# item_damage_1 = float(item_damage_1_input)
-# # This line converts the percent into a decimal 
-# item_damage_1_dec = item_damage_1 / 100
